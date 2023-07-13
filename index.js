d5530d737194310c876e7a168d13f46b0460b864
humphreynjuguna <humphreynjuguna98@gmail.com>
2023-07-14 00:02:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+    }
+    const mondayWork = function(activity = "go to the office"){
+        return `This Monday, I will ${activity}.`;
+    }
+    mondayWork();
+    
+    function wrapAdjective(string='*'){
+        return function(adj='special'){
+            return `You are ${string}${adj}${string}!`
+    
+        }
+    }
+    wrapAdjective()();
+    
